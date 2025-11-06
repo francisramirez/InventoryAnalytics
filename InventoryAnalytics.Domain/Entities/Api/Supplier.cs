137350567d6b978ce58c2fa00d6a3180e2d19011
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2025-11-06 19:04:04 -0400

    consumo del api agretado.

--- InventoryAnalytics.Domain/Entities/Api/Supplier.cs
+++ InventoryAnalytics.Domain/Entities/Api/Supplier.cs
@@ -3,5 +3,7 @@ namespace InventoryAnalytics.Domain.Entities.Api
 {
     public class Supplier
     {
+        public int supplierId { get; set; }
+        public string? supplierName { get; set; }
     }
 }
--- InventoryAnalytics.Persistence/Repositories/Api/SupplierApiRepository.cs
+++ InventoryAnalytics.Persistence/Repositories/Api/SupplierApiRepository.cs
@@ -5,12 +5,54 @@ namespace InventoryAnalytics.Persistence.Repositories.Api
 {
     using InventoryAnalytics.Application.Repositories;
     using InventoryAnalytics.Domain.Entities.Api;
+    using Microsoft.Extensions.Logging;
+    using System.Net.Http.Json;
 
     public class SupplierApiRepository : ISupplierApiRepository
     {
-        public Task<IEnumerable<Supplier>> GetSuppliersAsync()
+       private readonly IHttpClientFactory _clientFactory;
+        private readonly ILogger<SupplierApiRepository> _logger;
+
+        public SupplierApiRepository(IHttpClientFactory clientFactory, ILogger<SupplierApiRepository> logger)
         {
-            throw new NotImplementedException();
+            _clientFactory = clientFactory;
+            _logger = logger;
+        }
+        public async Task<IEnumerable<Supplier>> GetSuppliersAsync()
+        {
+
+            List<Supplier> suppliers = new List<Supplier>();
+
+
+            try
+            {
+                using var client = _clientFactory.CreateClient("SupplierApiClient");
+                client.BaseAddress = new Uri("https://localhost:7299/");
+           
+                using var response = await client.GetAsync("api/SupplierApi/GetAllSuppliers");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var apiResponse = await response.Content.ReadFromJsonAsync<IEnumerable<Supplier>>();
+                    if (apiResponse != null)
+                    {
+                        suppliers = apiResponse.ToList();
+                    }
+                }
+                else
+                {
+                    _logger.LogError("Failed to fetch suppliers. Status Code: {StatusCode}", response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                suppliers = null!;
+                _logger.LogError(ex, "Error fetching suppliers from API");
+
+            }
+            return suppliers!;
+
+
         }
     }
 }
